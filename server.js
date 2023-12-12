2976454d19a609cdb0c97b46df7c35c008da8d96
“Buddy-Masri” <abdel.rahman.almasri@gmail.com>
2023-12-12 18:31:19 +0200

    save

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const corsOptions = {
-  origin: "https://buddy-masri.github.io/store/cart",
+  origin: "https://buddy-masri.github.io",
   credentials: true,
 };
 const bodyparser = require("body-parser");
